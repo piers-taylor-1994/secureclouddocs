c1fe05418551e166df9f28725c3db63890bf80b9
Piers Taylor <piers.taylorr@gmail.com>
2025-08-11 16:04:19 +0100

    Tweaked endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from dotenv import load_dotenv
 import os
 import boto3
@@ -12,6 +12,26 @@ s3_client = boto3.client(
     region_name=os.getenv("AWS_REGION")
 )
 
+def created_presigned_upload_url(filename, content_type):
+    bucket = os.getenv("S3_BUCKET")
+    key = f"uploads/{filename}"
+
+    url = s3_client.generate_presigned_url(
+        "put_object",
+        Params={
+            "Bucket": bucket, 
+            "Key": key, 
+            "ContentType": content_type},
+        ExpiresIn=300  # 5 minutes
+    )
+
+    return {
+        "url": url,
+        "expires_at": datetime.now(timezone.utc).isoformat() + "Z",
+        "key": key,
+        "content_type": content_type
+    }
+
 @app.get("/health")
 def read_health():
     return {
@@ -21,15 +41,8 @@ def read_health():
 
 @app.get("/generate-upload-url")
 def generate_upload_url(filename):
-    bucket = os.getenv("S3_BUCKET")
-    key = f"uploads/{filename}"
-    url = s3_client.generate_presigned_url(
-        "put_object",
-        Params={"Bucket": bucket, "Key": key},
-        ExpiresIn=300  # 5 minutes
-    )
-    return {
-        "url": url,
-        "expires_at": datetime.now(timezone.utc).isoformat() + "Z",
-        "key": key
-    }
+    return created_presigned_upload_url(filename, "application/octet-stream")
+
+@app.post("/upload")
+def upload_file(filename: str = Body( ... ), content_type: str = Body( ... )):
+    return created_presigned_upload_url(filename, content_type)
