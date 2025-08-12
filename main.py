41dce9a661ff5e5e2bc11882fc33c81254faf9a2
Piers Taylor <piers.taylorr@gmail.com>
2025-08-12 13:13:06 +0100

    Added more logging + added exception handler

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timezone
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
+from fastapi.responses import JSONResponse
 from aws import create_presigned_upload_url
 from logging_config import *
 from validators import *
 import logging
+import traceback
 
 #fastapi dev main.py
 
@@ -14,6 +16,7 @@ app = FastAPI()
 
 @app.get("/health")
 def read_health():
+    logger.info(f"Health check requested")
     return {
         "status":"ok", 
         "time":datetime.now(timezone.utc).isoformat() + "Z"
@@ -21,6 +24,7 @@ def read_health():
 
 @app.get("/generate-upload-url")
 def generate_upload_url(filename):
+    logger.info(f"Presigned upload url requested for file '{filename}'")
     return create_presigned_upload_url(filename, "application/octet-stream")
 
 @app.post("/upload")
@@ -30,3 +34,16 @@ def upload_file(filename: str = Body( ... ), content_type: str = Body( ... )):
     validate_content_type(content_type)
 
     return create_presigned_upload_url(filename, content_type)
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(json.dumps({
+        "error": str(exc),
+        "path": request.url.path,
+        "method": request.method,
+        "trace": traceback.format_exc()
+    }))
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal server error"}
+    )
